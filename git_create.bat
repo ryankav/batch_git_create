5f8deae5d2f1e72d2c82a05740a9761d71e7f64d
ryan_kav <ryan.p.kav@gmail.com>
2020-09-02 18:57:52 +0100

    Simplified Batch File
    Simplified Batch file so that the Github API
    automatically creates the README.md and
    LISCENCE.md file. Then clone the repository made
    into the local directory.

--- git_create.bat
+++ git_create.bat
@@ -1,12 +1,4 @@
 @echo off
-curl -i -H "Authorization: token %GITHUB_TOKEN%" -d "{\"name\": \"%1\", \"license_template\": \"apache-2.0\"}" https://api.github.com/user/repos
+curl -i -H "Authorization: token %GITHUB_TOKEN%" -d "{\"name\": \"%1\", \"license_template\": \"apache-2.0\", \"auto_init\": true}" https://api.github.com/user/repos
 cd C:\Users\Ryan Kavanagh\git_projects
-mkdir %1
-cd %1
-git init
-type nul > README.md
-git add README.md
-git commit -m "first commit"
-git branch -M master
-git remote add origin https://github.com/ryankav/%1.git
-git push -u origin master
+git clone https://github.com/ryankav/%1.git
